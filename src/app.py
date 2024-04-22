8218eb7c5641d5a4c660b611632b6cbe595ebae8
Admir Skomorac <admir.skomorac@outlook.com>
2024-04-22 20:57:05 +0000

    new table Planets added

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +36,108 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+@app.route('/planets', methods=['GET'])
+def get_all_items():
+    if request.path == '/people':
+        items = People.query.all()
+    elif request.path == '/planets':
+        items = Planets.query.all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    serialized_items = [item.serialize() for item in items]
+    return jsonify(serialized_items), 200
 
-    return jsonify(response_body), 200
+@app.route('/people/<int:person_id>', methods=['GET'])
+def get_person(person_id):
 
+    person = People.query.get(person_id)
+    if person is None:
+        return "Person with id: " + str(person_id) + " doesn't exists", 400
+    one_person = person.serialize()
+
+    return jsonify(one_person), 200
+
+
+@app.route('/people', methods=['POST'])
+def create_person():
+
+    new_person = request.get_json()
+
+    if 'name' not in new_person:
+        return "Name cannot be empty", 400
+    
+
+    new_person = People(
+        name = new_person['name'], 
+        height = new_person['height'],
+        mass = new_person['mass'],
+        hair_color = new_person['hair_color'],
+        skin_color = new_person['skin_color'],
+        eye_color = new_person['eye_color'],
+        birth_year = new_person['birth_year'],
+        gender = new_person['gender'],
+        homeworld = new_person['homeworld'],
+    )
+
+    db.session.add(new_person)
+    db.session.commit()
+
+    return jsonify({"msg": "New Person is created"}), 201
+
+
+@app.route('/people/<int:person_id>', methods=['PUT'])
+def update_person(person_id):
+    
+    updated_person = request.get_json()
+    old_person = People.query.get(person_id)
+
+    if old_person is None:
+        return "Person with id: " + str(person_id) + " doesn't exist", 400 
+    
+    if 'name' in updated_person:
+        old_person.name = updated_person['name']
+
+    if 'birth_year' in updated_person:
+        old_person.birth_year = updated_person['birth_year']
+
+    if 'eye_color' in updated_person:
+        old_person.eye_color = updated_person['eye_color']
+
+    if 'gender' in updated_person:
+        old_person.gender = updated_person['gender']
+
+    if 'hair_color' in updated_person:
+        old_person.hair_color = updated_person['hair_color']
+
+    if 'height' in updated_person:
+        old_person.height = updated_person['height']
+
+    if 'homeworld' in updated_person:
+        old_person.homeworld = updated_person['homeworld']
+
+    if 'mass' in updated_person:
+        old_person.mass = updated_person['mass']
+
+    if 'skin_color' in updated_person:
+        old_person.skin_color = updated_person['skin_color']
+
+    db.session.commit()
+
+    return jsonify({"msg": "Person is updated"}), 200
+
+
+@app.route('/people/<int:person_id>', methods=['DELETE'])
+def delete_person(person_id):
+    person_to_delete = People.query.get(person_id)
+
+    if person_to_delete is None:
+        return "Person with id: " + str(person_id) + " doesn't exists", 400
+    
+    db.session.delete(person_to_delete)
+    db.session.commit()
+
+    return jsonify({"msg": "Person is deleted"}, 200)
+        
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -2,18 +2,64 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(100))
+    skin_color = db.Column(db.String(100))
+    eye_color = db.Column(db.String(100))
+    birth_year = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    homeworld = db.Column(db.String(250))
+
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+            # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+
+
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
             # do not serialize the password, its a security breach
         }
